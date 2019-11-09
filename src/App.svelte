7f3a6c8651f9ba2575e631e97113cefcb4479129
Jeffrey Vest <jeffv@aspenrxhealth.com>
2019-11-08 20:03:28 -0500

    Added the sounds!

--- src/App.svelte
+++ src/App.svelte
@@ -2,41 +2,32 @@
   import * as tmImage from "@teachablemachine/image";
   import { onMount } from "svelte";
   import _ from "lodash";
+  import Tone from "tone";
 
-  // More API functions here:
-  // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
-
-  // the link to your model provided by Teachable Machine export panel
-  const URL = "https://teachablemachine.withgoogle.com/models/tUHecTMi/";
+  const URL = "https://teachablemachine.withgoogle.com/models/CBBvJbaj/";
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
 
   let model;
   let webcam;
-  let webcamContainera;
-  let labelContainer;
-  let maxPredictions;
+  let webcamContainer;
   let predictedClass = "";
+  let note;
+  let osc;
+  let stopped = true;
 
   // Load the image model and setup the webcam
   onMount(async function() {
-    const modelURL = URL + "model.json";
-    const metadataURL = URL + "metadata.json";
-
-    // load the model and metadata
-    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-    // or files from your local hard drive
-    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
     model = await tmImage.load(modelURL, metadataURL);
-    maxPredictions = model.getTotalClasses();
 
-    // Convenience function to setup a webcam
-    const flip = true; // whether to flip the webcam
-    webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
-    await webcam.setup(); // request access to the webcam
+    webcam = new tmImage.Webcam(200, 200, true);
+    await webcam.setup();
     await webcam.play();
     window.requestAnimationFrame(loop);
 
-    // append elements to the DOM
-    webcamContainera.appendChild(webcam.canvas);
+    webcamContainer.appendChild(webcam.canvas);
+
+    osc = new Tone.Oscillator(440, "sawtooth3").toMaster();
   });
 
   async function loop() {
@@ -51,7 +42,26 @@
     const prediction = await model.predict(webcam.canvas);
     predictedClass = _.maxBy(prediction, x => x.probability).className;
   }
+
+  $: {
+    if (predictedClass === "high") note = 500;
+    else if (predictedClass === "mid") note = 400;
+    else if (predictedClass === "low") note = 300;
+  }
+
+  $: if (osc) osc.frequency.linearRampTo(note, 0.1);
+
+  $: {
+    if (osc) {
+      if (stopped) osc.stop();
+      else osc.start();
+    }
+  }
 </script>
 
-<div bind:this={webcamContainera} />
-<div>{predictedClass}</div>
+<div bind:this={webcamContainer} />
+{#if osc}
+  <button on:click={() => (stopped = !stopped)}>
+    {stopped ? 'Start' : 'Stop'}
+  </button>
+{/if}
